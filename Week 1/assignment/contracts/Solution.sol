25e3056c553ad831706059f403f213397665f49e
paulgi48 <paulgiovannio@outlook.com>
2022-10-15 02:28:15 +0100

    Update Solution.sol

--- Week 1/assignment/contracts/Solution.sol
+++ Week 1/assignment/contracts/Solution.sol
@@ -1,7 +1,7 @@
 // Upgrade the ether smart allet smart contract to allow ...
 // individuals withdraw any amount of etherereum ... they deposited in the smart contract.
 
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.7;
 
 contract EtherWallet {
     address payable public owner;
@@ -14,16 +14,17 @@ contract EtherWallet {
 
     function deposit() external payable {
         require(msg.sender);
-        payable ()
-        
+        payable () ;
     }
 
-    function withdraw(uint _amount) external {
+    function withdraw(uint , any amount) external {
         require(msg.sender == owner, "caller is not owner");
-        payable(msg.sender).transfer(_amount);
+        payable(msg.sender).transfer(any amount);
     }
 
     function getBalance() external view returns (uint) {
         return address(this).balance;
     }
+
+
 }
