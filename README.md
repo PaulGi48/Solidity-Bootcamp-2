28c8201f516d6ac497f2ad91ba782cf165da6d20
paulgi48 <paulgiovannio@outlook.com>
2022-10-21 13:53:17 +0100

    Assignment Week 2

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# Ekolance Solidity-Bootcamp-2
-Week 1 - Learnine Remix.IDE and building smart contracts
+##  Ekolance Solidity-Bootcamp-2
+Week 1 - Learning Web3 , Blockchain, Remix.IDE and building smart contracts.
+
+Week 2 - Working on Solidity and testing my code;
+Data Location,Functions,The Constructor,Visibility Modifiers,Function Modifiers,
+Error Handling,Events,Inheritance,Shadowing inherited state variables,
+Calling parent contracts,Interfaces,Importing,LibraryS.
+
+
+
 
--- Week 2/assignment/contracts/Solution.sol
+++ Week 2/assignment/contracts/Solution.sol
@@ -1,12 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.0 <0.9.0;
+
+//Assignment 2
 // Upgrade the ether smart allet smart contract to allow ...
 // individuals withdraw any amount of etherereum ... they deposited in the smart contract.
 
 
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
-
 contract EtherWallet {
-    address payable owner;
+    address payable public owner;
 
         modifier ownerOnly() {
         require(msg.sender == owner, "only the owner can call this function");
@@ -24,16 +25,17 @@ contract EtherWallet {
     }
   
     //withdraw an amount
-    function withdraw(uint256 _amount) external {
-        require(msg.sender == owner, "caller is not owner");
+    function withdraw(uint256 _amount) public  {
+        require(msg.sender == owner, "caller can be owner");
         payable(msg.sender).transfer(_amount);
     }
 
-
     // view your balance
-    function getBalance() external view returns (uint256) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
+
+
 }
 
 
